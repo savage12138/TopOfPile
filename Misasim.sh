0c8213fbf1cfc8d2b6d4b6172dddf8a03d11fb93
savage12138 <yida12138@gmail.com>
2020-09-04 18:06:05 -0400

    Misasim script file

---
+++ Misasim.sh
@@ -0,0 +1,8 @@
+# cd to your Misasim installation bin directory
+cd ../Misasim/bin
+# Launching Misasim from terminal or from Windows10 
+# Bash shell: set up an X server to support the GUI. 
+# You need to set the DISPLAY variable in the terminal 
+# command line and then launch Misasim:
+export DISPLAY=:0
+python3 Misasim.py
